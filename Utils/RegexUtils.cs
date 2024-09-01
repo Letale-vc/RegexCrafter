087df4585264b2b9229a9ab68fced74381160303
Helpless <letalerv@gmail.com>
2024-09-01 21:38:52 +0300

    refactor: Update RegexUtils to support multiline patterns

--- Utils/RegexUtils.cs
+++ Utils/RegexUtils.cs
@@ -1,40 +1,55 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-namespace RegexCrafter.Utils;
-public class RegexUtils
+namespace RegexCrafter.Utils
 {
-	public static bool MatchesPattern(string text, string pattern)
+	public class RegexUtils
 	{
-		var regex = new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Multiline);
-		return regex.IsMatch(text);
-	}
-
-	public static bool MatchesAnyPattern(string text, string[] patterns, out List<string> applyPatterns)
-	{
-		applyPatterns = [];
-		foreach (string pattern in patterns)
+		public static bool MatchesPattern(string text, string pattern)
 		{
-			if (MatchesPattern(text, pattern))
+			var regex = new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+			return regex.IsMatch(text);
+		}
+		public static bool MatchesAnyPattern(string text, string[] patterns, out List<string> applyPatterns)
+		{
+			applyPatterns = new List<string>();
+			var lines = text.Split(new[] { '\r', '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+			foreach (string pattern in patterns)
 			{
+				foreach (string line in lines)
+				{
+					if (MatchesPattern(line, pattern))
+					{
+						applyPatterns.Add(pattern);
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		public static bool MatchesAllPatterns(string text, string[] patterns, out List<string> applyPatterns)
+		{
+			applyPatterns = new List<string>();
+			var lines = text.Split(new[] { '\r', '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+			foreach (string pattern in patterns)
+			{
+				bool patternMatched = false;
+				foreach (string line in lines)
+				{
+					if (MatchesPattern(line, pattern))
+					{
+						patternMatched = true;
+						break;
+					}
+				}
+				if (!patternMatched)
+				{
+					return false;
+				}
 				applyPatterns.Add(pattern);
-				return true;
 			}
+			return true;
 		}
-		return false;
-	}
-
-	public static bool MatchesAllPatterns(string text, string[] patterns, out List<string> applyPatterns)
-	{
-		applyPatterns = [];
-		foreach (string pattern in patterns)
-		{
-			if (!MatchesPattern(text, pattern))
-			{
-				return false;
-			}
-			applyPatterns.Add(pattern);
-		}
-		return true;
 	}
 }
